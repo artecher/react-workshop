10716275b49d0847f771698c2bb187afcfe76b33
ethan.artecher <ethanleenz@gmail.com>
2017-07-18 16:30:21 +0800

    MASTER: Remove all ;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 import AddTask from './components/add-task/AddTask'
 import TaskItem from './components/task-item/TaskItem'
 
@@ -85,8 +85,8 @@ class App extends Component {
         </div>
 
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
